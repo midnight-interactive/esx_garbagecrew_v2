124e4b74f0b87baafb7d7fa579fc20c53cf7f08d
TJ <tjspann@users.noreply.github.com>
2020-10-01 13:53:28 -0500

    Update fxmanifest.lua
    RageUI

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,28 @@ fx_version 'adamant'
 
 game 'gta5'
 
+description 'RageUI ESX Garbage Crew'
+
+version '1.0.2'
+
+client_scripts {
+    "src/client/RMenu.lua",
+    "src/client/menu/RageUI.lua",
+    "src/client/menu/Menu.lua",
+    "src/client/menu/MenuController.lua",
+
+    "src/client/components/*.lua",
+
+    "src/client/menu/elements/*.lua",
+
+    "src/client/menu/items/*.lua",
+
+    "src/client/menu/panels/*.lua",
+
+    "src/client/menu/windows/*.lua",
+
+}
+
 server_scripts {
 	'@es_extended/locale.lua',
 	'locales/cs.lua',
